8c858845aef490845d4e3670f97e3081a38368fa
Anderson Carvalho <andersonccosta.devops@gmail.com>
2025-01-21 09:17:52 -0300

    create: commit inicial

---
+++ Infra/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.8.0"
+
+    }
+  }
+}
+
+provider "google" {
+  project = ""
+  region  = ""
+  zone    = ""
+}
